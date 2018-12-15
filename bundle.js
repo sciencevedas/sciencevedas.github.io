85ea61507b342603a9df5a316f56fd305d2627bd
ElectricNinja315 <suzac314@gmail.com>
2018-12-15 15:40:25 -0800

    asdf

--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (process){
 // .dirname, .basename, and .extname methods are extracted from Node.js v8.11.1,
 // backported and transplited with Babel, with backwards-compat fixes
@@ -611,7 +611,7 @@ module.exports=[
 {"atomicNumber":117,"symbol":"Ts","name":"Tennessine","atomicMass":[294],"cpkHexColor":"","electronicConfiguration":"[Rn] 5f14 6d10 7s2 7p5","electronegativity":"","atomicRadius":"","ionRadius":"","vanDelWaalsRadius":"","ionizationEnergy":"","electronAffinity":"","oxidationStates":"","standardState":"","bondingType":"","meltingPoint":"","boilingPoint":"","density":"","groupBlock":"post-transition metal","yearDiscovered":2010},
 {"atomicNumber":118,"symbol":"Og","name":"Oganesson","atomicMass":[294],"cpkHexColor":"","electronicConfiguration":"[Rn] 5f14 6d10 7s2 7p6","electronegativity":"","atomicRadius":"","ionRadius":"","vanDelWaalsRadius":"","ionizationEnergy":"","electronAffinity":"","oxidationStates":"","standardState":"","bondingType":"","meltingPoint":"","boilingPoint":"","density":"","groupBlock":"noble gas","yearDiscovered":2002}
 ]
-},{}],4:[function(require,module,exports){
+},{}],"periodic-table":[function(require,module,exports){
 (function (__dirname){
 
 var path = require('path');
@@ -641,9 +641,4 @@ module.exports.numbers = data.reduce(function(obj, element) {
 
 
 }).call(this,"/node_modules/periodic-table")
-},{"./data":3,"path":1}],5:[function(require,module,exports){
-var pt = require('periodic-table');
-
-console.log('hello world');
-console.log(pt.all);
-},{"periodic-table":4}]},{},[5]);
+},{"./data":3,"path":1}]},{},[]);
--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,25 @@
-	var pt = require('periodic-table');
-	var myApp = angular.module('myApp', []);
-
-		angular.forEach(pt.symbols, function(value, key) {
-			if(chemical_symbol == value.key)
-				results.innerHTML=pt.symbols.chemical_symbol;
-		});
-
-
+var pt = require('periodic-table');
 
 console.log('hello world');
 console.log(pt.all);
+
+function results() {
+    element = document.getElementById('element').value;
+    console.log(element.toLowerCase());
+
+    // loop through database
+    const keys = Object.keys(pt.symbols);
+    for (const key of keys) {
+        console.log(key);
+        if (key.symbol.toString().toLowerCase() == element) {
+            console.log("FOUND IT");
+        }
+    }
+
+    // find element
+
+    // parse through data
+}
 
 // loop through pt.all dataset
 // find a  match 
