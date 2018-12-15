01e06fa49d2a1adc427a2eadb92cd95c35eb1adc
ElectricNinja315 <suzac314@gmail.com>
2018-12-15 11:54:25 -0800

    delete useless js

---
+++ scripts/chem.py
@@ -0,0 +1,15 @@
+import cgi, cgitb
+
+form = cgi.FieldStorage()
+
+chemical_symbol = form.getvalue('chemical_symbol')
+
+print ("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Hello - Second CGI Program</title>")
+print ("</head>")
+print ("<body>")
+print ("<h2>Your chemical is %s</h2>" % chemical_symbol)
+print ("</body>")
+print ("</html>")
