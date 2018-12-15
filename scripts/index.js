b011a8419d38ce9c575c17381ff5ae73fdde7ec9
mihir46 <mihirtyagi8@gmail.com>
2018-12-15 15:15:24 -0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,12 @@
-var pt = require('periodic-table');
+	var pt = require('periodic-table');
+	var myApp = angular.module('myApp', []);
+
+		angular.forEach(pt.symbols, function(value, key) {
+			if(chemical_symbol == value.key)
+				results.innerHTML=pt.symbols.chemical_symbol;
+		});
+
+
 
 console.log('hello world');
 console.log(pt.all);
