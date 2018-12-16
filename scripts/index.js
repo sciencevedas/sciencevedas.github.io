60c140154ce6ca52e61ec7c5f64a3f47458c4923
ElectricNinja315 <suzac314@gmail.com>
2018-12-15 16:52:57 -0800

    extend capabilities

--- scripts/index.js
+++ scripts/index.js
@@ -28,8 +28,49 @@ function results() {
             elementinfo.innerHTML = output.join("<br>");
         }
     }
+    for (const qey of pt.all()) {
+        if (qey.name.toLowerCase() == element.toLowerCase()) {
+            console.log("FOUND IT");
+            console.log(qey.atomicNumber);
+            console.log(qey.name);
+            console.log(qey.electronegativity);
 
+            // put onto html
+            output = []
 
+            output.push(`Symbol: ${qey.symbol}`);
+            output.push(`Atomic no: ${qey.atomicNumber}`);
+            output.push(`Electronegativity: ${qey.electronegativity}`);
+            output.push(`Type: ${qey.groupBlock}`);
+            output.push(`IE: ${qey.ionizationEnergy} kJ/mol`);
+            output.push(`e- affinity: ${qey.electronAffinity} kJ/mol`);
+            output.push(`Electron configuration: ${qey.electronicConfiguration}`);
+
+            elementinfo.innerHTML = output.join("<br>");
+        }
+    }
+
+    for (const qey of pt.all()) {
+        if (qey.atomicNumber == element) {
+            console.log("FOUND IT");
+            console.log(qey.atomicNumber);
+            console.log(qey.name);
+            console.log(qey.electronegativity);
+
+            // put onto html
+            output = []
+
+            output.push(`Symbol: ${qey.symbol}`);
+            output.push(`Name: ${qey.name}`);
+            output.push(`Electronegativity: ${qey.electronegativity}`);
+            output.push(`Type: ${qey.groupBlock}`);
+            output.push(`IE: ${qey.ionizationEnergy} kJ/mol`);
+            output.push(`e- affinity: ${qey.electronAffinity} kJ/mol`);
+            output.push(`Electron configuration: ${qey.electronicConfiguration}`);
+
+            elementinfo.innerHTML = output.join("<br>");
+        }
+    }
 }
 
 // loop through pt.all dataset
