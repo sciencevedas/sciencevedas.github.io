52f8d7f0c2938515b67afe6aeb214e43f3446e16
ElectricNinja315 <suzac314@gmail.com>
2018-12-15 16:37:15 -0800

    IT WORKS

--- scripts/index.js
+++ scripts/index.js
@@ -8,16 +8,36 @@ function results() {
     console.log(element.toLowerCase());
 
     // loop through database
+    /*
     const keys = Object.keys(pt.symbols);
     for (const key of keys) {
         console.log(String(key).toLowerCase() + " " + element.toLowerCase())
         if (String(key).toLowerCase() == element.toLowerCase()) {
             console.log("FOUND IT");
+            console.log(pt.symbols.key);
+
+            const keys2 = Object.keys(key);
+            for (const key2 of keys2) {
+                console.log(key2 + " " + key.value);
+            }
+            console.log('==========================');
+        }
+    }
+    */
+
+    console.log("-=567890-=-098767890-=-0987890-=")
+
+    for (const qey of pt.all()) {
+        console.log(qey);
+        if (qey.symbol.toLowerCase() == element.toLowerCase()) {
+            console.log("FOUND IT");
+            console.log(qey.atomicNumber);
+            console.log(qey.name);
+            console.log(qey.electronegativity);
         }
     }
 
 
-    // parse through data
 }
 
 // loop through pt.all dataset
